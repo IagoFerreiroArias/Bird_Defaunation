6fb0f7ff633ba26e38bb9239c17c08ef784e3928
IagoFerreiroArias <iago.ferreiro.arias@gmail.com>
2023-05-12 10:22:31 +0200

    Update

--- Scripts/Data_Cleaning.R
+++ Scripts/Data_Cleaning.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Script: Clean hunting database: Birds dataset
 # Author: Iago Ferreiro Arias
 # Date: 3th October 2022
@@ -146,3 +147,153 @@ write.csv(RR_data, "Data/Bird_RR_data.csv", row.names=F)
 
 
 
+=======
+# Script: Clean hunting database: Birds dataset
+# Author: Iago Ferreiro Arias
+# Date: 3th October 2022
+
+###############################################################################
+library(dplyr)
+library(writexl) #export results into xlsx files
+library(sp)
+library(mapview)
+##############################################################################
+
+
+# Import hunting-induced defaunation data:
+hunting_raw <- read.csv("Data/Hunting/Defaunation_db.csv", sep=";")
+
+levels(as.factor(hunting_raw$Analisis))
+hunting_raw <- subset(hunting_raw, Analisis=="yes") #remove rows not included in analisis
+
+levels(as.factor(hunting_raw$Hloss_Fragm))
+hunting_raw <- subset(hunting_raw, Hloss_Fragm=="None") #remove rows with potential confounding effects
+
+levels(as.factor(hunting_raw$Group)) #filter bird species
+hunting_birds <- subset(hunting_raw, Group=="Birds")
+rm(hunting_raw) #clean environment
+
+hunting_birds
+
+#Create response variables
+hunting_birds$RR <-hunting_birds$Hdens/hunting_birds$Udens # continuous models 
+hunting_birds$bin <- ifelse(hunting_birds$Hdens == 0, 0, 1) # binomial models
+
+#Create FoodBiomass predictor
+hunting_birds$Cattle_biomass <- hunting_birds$CattleDens * 459.6
+hunting_birds$Pig_biomass <- hunting_birds$PigDens * 117.5
+hunting_birds$Chick_biomass<- hunting_birds$ChickDens * 2.112
+hunting_birds$Goat_biomass <- hunting_birds$GoatDens * 54.9
+hunting_birds$Sheep_biomass <- hunting_birds$SheepDens* 54.9
+
+hunting_birds$FoodBiomass <- hunting_birds$Cattle_biomass + hunting_birds$Pig_biomass + 
+  hunting_birds$Chick_biomass + hunting_birds$Goat_biomass + hunting_birds$Sheep_biomass 
+
+#Select columns of interest
+
+hunting_birds <- hunting_birds %>% dplyr::select(Study, Dataset=dataset2,
+                                                Species, Nspp, RR, bin, 
+                                                Hdens, Udens,
+                                                Hunting=Type_of_hu, 
+                                                Realm=Loc2,
+                                                Country, Longitude, Latitude,
+                                                Dist_Hunters = InterDist,
+                                                TravDist, PopDens=PopDensUpd,
+                                                Stunting= stunting2, FoodBiomass, 
+                                                Reviewer, Reserve)
+
+
+hunting_birds$Dist_Hunters <- as.numeric(hunting_birds$Dist_Hunters)/1000
+
+table(hunting_birds$Nspp) #>200 ratio are grouped species (Eg. Mitu spp.)
+
+hunting_birds <- subset(hunting_birds, Nspp==1)
+
+#Import bird species taxonomy crosswalk
+crosswalk <- read.csv("Data/Hunting/Crosswalk_Birds.csv", sep=";")
+crosswalk <- crosswalk %>% dplyr::select(Species, BirdLife_Species, BirdTree_Species,
+                                         IUCN_Species, IUCN_Status)
+
+which(duplicated(crosswalk$Species))
+hunting_birds <- left_join(hunting_birds, crosswalk, by="Species")
+which(is.na(hunting_birds$IUCN_Status)) #check NAs
+
+hunting_birds <- subset(hunting_birds, Udens!=0)
+
+#Spatial overview
+spatial <- SpatialPointsDataFrame(cbind(hunting_birds$Longitude, hunting_birds$Latitude),
+                                  data=hunting_birds, 
+                                  proj4string=CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+
+mapview(spatial, zcol="Reviewer", layer.name="Reviewer")
+rm(spatial)
+
+
+# Select specific traits (dataframe from Traits_Birds.R)
+
+bird_traits <- TRAITS %>% dplyr::select(Order, Family, BirdTree_Species,
+                                        Body_Mass, HWI, Kipps_Dist, Lifestyle,
+                                        Trophic_Level, Activity, PCA_Dim1,
+                                        PCA_Dim2, Evo_Distinctiveness,
+                                        Tax_Distinctiveness, Traded)
+
+bird_traits <- bird_traits[-which(duplicated(bird_traits$BirdTree_Species)),]#remove duplicated 
+bird_traits[which(bird_traits$BirdTree_Species=="Stachyris chrysaea"),"BirdTree_Species"] <- "Cyanoderma chrysaeum"
+#change name: Stachyris chrysaea == Cyanoderma chrysaeum
+hunting_birds <- left_join(hunting_birds, bird_traits, by="BirdTree_Species")
+
+which(is.na(hunting_birds$Traded)) #check join
+
+hunting_birds$Realm <- ifelse(hunting_birds$Realm=="Africa", "Afrotropic",
+                              ifelse(hunting_birds$Realm=="Asia", "Indomalayan", 
+                                     "Neotropic"))
+
+hunting_birds$Trophic_Level <- ifelse(hunting_birds$Trophic_Level=="Carnivore", "Carnivore",
+                              ifelse(hunting_birds$Trophic_Level=="Omnivore", "Omnivore", 
+                              ifelse(hunting_birds$Trophic_Level=="Herbivore", "Herbivore", 
+                                     "Carnivore")))
+
+hunting_birds <- hunting_birds %>% dplyr::select(Study, Dataset, Reviewer, Order, Family,
+                                          Species, BirdLife_Species, BirdTree_Species,
+                                          IUCN_Species, IUCN_Status,Hunting,RR, bin, 
+                                          Hdens, Udens,Traded, Body_Mass, HWI, Kipps_Dist,
+                                          Lifestyle, Trophic_Level, Activity,
+                                          PCA_Dim1, PCA_Dim2, Evo_Distinctiveness,
+                                          Tax_Distinctiveness, Realm,
+                                          Country, Longitude, Latitude, Dist_Hunters,
+                                          TravDist, PopDens, Stunting, FoodBiomass,
+                                          Reserve)
+
+length(unique(hunting_birds$Species))
+
+hunting_birds$Reserve <- ifelse(hunting_birds$Reserve =="Yes", 1, 0)
+levels(as.factor(hunting_birds$Reserve))
+
+unique(hunting_birds$Country)
+
+#numerical code for countries
+countries <- read.csv("Data/Spatial/Countries.csv", sep=";")
+unique(hunting_birds$Country) %in% countries$COUNTRY
+countries <- countries %>% rename(Country= COUNTRY)
+hunting_birds <- left_join(hunting_birds, countries, by="Country")
+rm(countries)
+
+#create dataset for modelling
+RR_data <- hunting_birds %>% dplyr::filter(RR !="-Inf") %>% dplyr::filter(Hunting!="None")
+
+#Spatial overview
+spatial <- SpatialPointsDataFrame(cbind(RR_data$Longitude, RR_data$Latitude),
+                                  data=RR_data, 
+                                  proj4string=CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"))
+
+mapview(spatial, zcol="Reviewer", layer.name="Reviewer")
+rm(spatial)
+
+
+rm(list=c("hunting_birds", "crosswalk", "bird_traits"))
+
+write.csv(RR_data, "Data/Bird_RR_data.csv", row.names=F)
+
+
+
+>>>>>>> Stashed changes
--- Scripts/Model_Visualization.R
+++ Scripts/Model_Visualization.R
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #Script: Model diagnosis and marginal effects
 #Author: Iago Ferreiro Arias
 #Date: 16th March, 2023
@@ -112,3 +113,119 @@ hu7 <- ggarrange(hu2,hu3,hu4,hu5,hu6)
 ggarrange(hu7, hu1)
 rm(list=c("hu1","hu2","hu3", "hu4","hu5","hu6","hu7", "hurdle_gam_int"))
 # interaction between distance to hunter's access point and body mass is not supported
+=======
+#Script: Model diagnosis and marginal effects
+#Author: Iago Ferreiro Arias
+#Date: 16th March, 2023
+
+library(brms)
+library(sjPlot)
+library(ggplot2)
+library(ggpubr)
+
+# import hurdle model
+load("Results/hurdle_gam.RData")
+
+# diagnosis plots
+
+p1<- brms::pp_check(hurdle_gam, ndraws=1000) + xlim(0,15)#1
+p2<- brms::pp_check(hurdle_gam, type="scatter_avg",ndraws=1000) #predicted vs observed
+p3<- brms::pp_check(hurdle_gam, type="stat_2d", ndraws=1000) # mean and sd of posterior predictions vs observed
+p4<- brms::pp_check(hurdle_gam, type="loo_pit", ndraws=1000) 
+ggpubr::ggarrange(p1,p2,p3,p4)
+
+rm(list=c("p1","p2","p3","p4"))
+
+
+# residuals vs fitted values
+RR_data2 <- hurdle_gam$data
+RR_data2$Residuals <- residuals(hurdle_gam)[,1]
+RR_data2$Fitted <- fitted(hurdle_gam)[,1]
+plot(RR_data2$Fitted, RR_data2$Residuals)
+rm(RR_data2)
+
+#marginal effects: changes in abundance
+
+mu1 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("BodyMass_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu2 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("DistHunt_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu3 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("TravDist_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu4 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("Stunting_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu5 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("PopDens_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu6 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("Reserve"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)") + ylim(-2,2)
+mu7 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="mu", terms=c("Traded"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)") + ylim(-2,2)
+ggarrange(mu1,mu2,mu3,mu4,mu5, mu6,mu7, nrow = 2, ncol=4) 
+
+rm(list=c("mu1","mu2","mu3", "mu4","mu5","mu6","mu7"))
+
+#marginal effects:  probability of local extirpation
+hu1 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("BodyMass_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("Probability of local extirpation") + ylim(0,1)
+hu2 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("DistHunt_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("Probability of local extirpation") + ylim(0,1)
+hu3 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("TravDist_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("Probability of local extirpation") + ylim(0,1)
+hu4 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("Stunting_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("Probability of local extirpation") + ylim(0,1)
+hu5 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("PopDens_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("Probability of local extirpation") + ylim(0,1)
+hu6 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("Reserve"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("Probability of local extirpation") + ylim(0,1)
+hu7 <- sjPlot::plot_model(hurdle_gam, type="pred",dpar="hu", terms=c("Traded"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("Probability of local extirpation") + ylim(0,1)
+ggarrange(hu1,hu2,hu3,hu4,hu5,hu6,hu7, nrow = 2, ncol=4) 
+rm(list=c("hu1","hu2","hu3", "hu4","hu5","hu6","hu7", "hurdle_gam"))
+
+
+##### Hurdle gam with interaction between body mass and distance to hunter's access points
+load("Results/hurdle_gam_int.RData")
+
+# diagnosis plots
+p1<- brms::pp_check(hurdle_gam_int, ndraws=1000) + xlim(0,15)#1
+p2<- brms::pp_check(hurdle_gam_int, type="scatter_avg",ndraws=1000) #predicted vs observed
+p3<- brms::pp_check(hurdle_gam_int, type="stat_2d", ndraws=1000) # mean and sd of posterior predictions vs observed
+p4<- brms::pp_check(hurdle_gam_int, type="loo_pit", ndraws=1000) 
+ggpubr::ggarrange(p1,p2,p3,p4)
+
+#warning messages: In smooth.construct.tp.smooth.spec(object, dk$data, dk$knots) : basis dimension, k, increased to minimum possible
+# interaction between body mass and distance to hunter's access point not supported?
+
+rm(list=c("p1","p2","p3","p4"))
+
+
+# residuals vs fitted values
+RR_data2 <- hurdle_gam_int$data
+RR_data2$Residuals <- residuals(hurdle_gam_int)[,1]
+RR_data2$Fitted <- fitted(hurdle_gam_int)[,1]
+plot(RR_data2$Fitted, RR_data2$Residuals)
+rm(RR_data2)
+
+
+#stablish reference values of body mass in order to plot interactions
+
+df_rescale <- read.csv("Results/Rescaling_values.csv") # import values for rescaling predictors
+(log10(3) - df_rescale[which(df_rescale$Predictor=="BodyMass"),"Mean"])/df_rescale[which(df_rescale$Predictor=="BodyMass"),"SD"] #4kg = 1.46
+(log10(2.5) - df_rescale[which(df_rescale$Predictor=="BodyMass"),"Mean"])/df_rescale[which(df_rescale$Predictor=="BodyMass"),"SD"] #2kg = 1.37
+(log10(0.7) - df_rescale[which(df_rescale$Predictor=="BodyMass"),"Mean"])/df_rescale[which(df_rescale$Predictor=="BodyMass"),"SD"] #700gr = 0.71
+(log10(0.03) - df_rescale[which(df_rescale$Predictor=="BodyMass"),"Mean"])/df_rescale[which(df_rescale$Predictor=="BodyMass"),"SD"] #30gr = -0.9
+(log10(0.015) - df_rescale[which(df_rescale$Predictor=="BodyMass"),"Mean"])/df_rescale[which(df_rescale$Predictor=="BodyMass"),"SD"] #30gr = -1.26
+rm(df_rescale) # Create interaction plots with three different values of body mass: 3kg (1.46), 700 gr (0.71) and 30 gr (-0.90)
+
+# marginal effects: changes in abundance
+mu1 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("DistHunt_log", "BodyMass_log[1.46,0.71,-0.9")) + 
+               theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2) + labs(colour="Body Mass") +
+               scale_color_discrete(labels = c("0.03kg","0.7kg", "2.5kg", "4kg"))
+mu2 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("TravDist_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu3 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("Stunting_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu4 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("PopDens_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(-2,2)
+mu5 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("Reserve"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)") + ylim(-2,2)
+mu6 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="mu", terms=c("Traded"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)") + ylim(-2,2)
+mu7 <- ggarrange(mu2,mu3,mu4,mu5,mu6) 
+ggarrange(mu7, mu1)
+
+# marginal effects: probability of local extirpation
+hu1 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("DistHunt_log", "BodyMass_log[1.46,0.71,-0.9")) + 
+  theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(0,1) + labs(colour="Body Mass") +
+  scale_color_discrete(labels = c("0.03kg","0.7kg", "2.5kg", "4kg"))
+hu2 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("TravDist_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(0,1)
+hu3 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("Stunting_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)")+ ylim(0,1)
+hu4 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("PopDens_log")) + theme_bw() + theme(plot.title = element_blank()) + ylab("log(RR)") + ylim(0,1)
+hu5 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("Reserve"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)")+ ylim(0,1)
+hu6 <- sjPlot::plot_model(hurdle_gam_int, type="pred",dpar="hu", terms=c("Traded"), dot.size=5) + theme_bw() + theme(plot.title = element_blank())+ ylab("log(RR)") + ylim(0,1)
+hu7 <- ggarrange(hu2,hu3,hu4,hu5,hu6) 
+ggarrange(hu7, hu1)
+rm(list=c("hu1","hu2","hu3", "hu4","hu5","hu6","hu7", "hurdle_gam_int"))
+# interaction between distance to hunter's access point and body mass is not supported
+>>>>>>> Stashed changes
